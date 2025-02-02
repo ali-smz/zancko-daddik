d47031fe563a609423d9c1ea581e03b869060bad
unknown <samiizadeh.ali@gmail.com>
2025-02-02 23:45:19 +0330

    pstgresql added

--- config/settings.py
+++ config/settings.py
@@ -86,12 +86,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'searchdb',
-        'USER': 'postgres',
-        'PASSWORD': 'yourpassword',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'searchdb',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'password',
+        # 'HOST': 'localhost',
+        # 'PORT': 5432,
     }
 }
 
