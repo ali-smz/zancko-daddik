29db7d2ae0c93c67dc60abb7a3532a4da41f9452
unknown <samiizadeh.ali@gmail.com>
2025-02-14 01:46:42 +0330

    env variables added

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1)pz7av(m96rpalz&_71d5e^$f%v)cy640ea@u(auttdq70!(n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG' , 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS' , '').split(',')
 
 
 # Application definition
