b8540f8be68909012a1ca65f6aab49a12042d27c
unknown <samiizadeh.ali@gmail.com>
2025-02-15 00:21:44 +0330

    security key updated

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1)pz7av(m96rpalz&_71d5e^$f%v)cy640ea@u(auttdq70!(n'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-1)pz7av(m96rpalz&_71d5e^$f%v)cy640ea@u(auttdq70!(n')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG' , 'False').lower() == 'true'
